92a55423f2320ab3ad6fde99ec6437f2cce8c040
Michelle-KW <77823806+Michelle-KW@users.noreply.github.com>
2021-02-04 06:50:54 +0300

    Add files via upload

---
+++ Python 1.py
@@ -0,0 +1,18 @@
+print ('hello world')
+print (237)
+print (103.9)
+print ("hello world")
+
+first_number= input('Enter a number: ')
+
+second_number= input('Enter another number: ')
+
+answer = float (first_number) + float (second_number)
+
+print(first_number + ' + ' + second_number + ' = ' + str(answer))
+
+first_name= input('Enter your first name:      ')
+
+last_name= input('Enter your last name:       ')
+
+print(first_name.capitalize() + ' ' + last_name.capitalize())
